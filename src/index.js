ec28666edf0dfca7ad9b5b3747245c7b92eb8721
VICTOR MITINGI <mitingi.victor@gmail.com>
2023-03-28 23:34:32 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // prevent the default form submit behavior
+    // handle the form submission here
+  });
 });
+
+  
